708261c4492a75878ab55d677713fcb56275cd7d
Dalia Rojas Vallejo <daliarojasvallejo@proton.me>
2024-06-22 15:31:08 -0500

    feat(nix): Configure Yazi

--- nix/homeModules/cli/yazi.nix
+++ nix/homeModules/cli/yazi.nix
@@ -14,6 +14,15 @@ in {
     programs.yazi = {
       enable = true;
       enableZshIntegration = true;
+
+      settings = {
+        manager = {
+          ratio = [1 3 4];
+        };
+        preview = {
+          image_filter = "nearest";
+        };
+      };
     };
   };
 }
